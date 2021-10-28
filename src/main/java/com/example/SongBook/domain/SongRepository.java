429fd2841d15c73b162d439a130fba8a4636750d
MikkoJaakola <mikko.jaakola@hotmail.fi>
2021-10-28 13:57:24 +0300

    Created SongRepository class

---
+++ src/main/java/com/example/SongBook/domain/SongRepository.java
@@ -0,0 +1,10 @@
+package com.example.SongBook.domain;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SongRepository extends CrudRepository <Song, Long> {
+
+	List<Song> findByTitle(String title);
+	
+}
