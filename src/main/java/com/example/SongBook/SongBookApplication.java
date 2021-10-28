4d4f4398b20322fb53ee8fa0ffd041c7bd9e307a
MikkoJaakola <mikko.jaakola@hotmail.fi>
2021-10-28 10:59:59 +0300

    Initial commit

---
+++ src/main/java/com/example/SongBook/SongBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.SongBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SongBook/SongBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SongBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
