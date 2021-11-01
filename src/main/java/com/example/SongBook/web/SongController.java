e519fb0b032033a01e78da4ffc9a9116179473d8
MikkoJaakola <mikko.jaakola@hotmail.fi>
2021-11-01 17:10:20 +0200

    Add song
    Created simple template page addsong.html for adding songs to the list

--- src/main/java/com/example/SongBook/web/SongController.java
+++ src/main/java/com/example/SongBook/web/SongController.java
@@ -1,12 +1,15 @@
 package com.example.SongBook.web;
 
 import java.util.List;
+
+import com.example.SongBook.domain.Song;
 import com.example.SongBook.domain.SongRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
@@ -20,4 +23,17 @@ public class SongController {
 		model.addAttribute("songs", repository.findAll());
 		return "songlist";
 	}
+	
+	@RequestMapping(value="/add")
+	public String addSong(Model model) {
+		model.addAttribute("song", new Song());
+		return "addsong";
+	}
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public String save(Song song) {
+		repository.save(song);
+		return "redirect:songlist";
+	}
+	
 }
